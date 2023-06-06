b2a905a656aada10724288e2efdb47c7cb141025
AlexisRios1316 <uranga127@gmail.com>
2023-06-05 21:37:12 -0600

    PROYECTOA FROND

---
+++ src/pages/Board.js
@@ -0,0 +1,6 @@
+// aCCESOS A LOS USUARIOS AUTENTICADOS 
+export const Board = () => {
+    return (
+        <h1> Dashboard </h1>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,62 @@
+// Login 
+import { useState } from "react";
+import axios from "axios"
+import { useNavigate } from "react-router-dom"; //Sirve para redireccionar un login 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form, Button, Alert} from 'react-bootstrap'
+
+
+
+//Generar componente de React 
+
+export const Home  = () => {
+
+
+   //Cambio de estado  para manejar los inputs  (DUDA) ///
+const [input,setInput] = useState(null)
+const navigate = useNavigate()  //permite navegar a otra ruta 
+
+
+const onChange  = (evt) => setInput({
+   /* Iteracion expander las iteraciones del objeto */
+...input,
+[evt.target.name] : evt.target.value
+
+})
+   const login = async (evt) => {
+
+      evt.preventDefault()
+const response  = await axios.post('http://127.0.0.1:5500/user', input)
+if(!response.data.IsAuth === false) return alert(response.data.message)
+return navigate('/Board')
+console.log(input)
+console.log(response.data.IsAuth)
+   }
+     return (
+      <Form onSubmit={login}>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control 
+    type="email" placeholder="Enter email" 
+    name="users"
+    onChange={onChange} />
+    
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control 
+    type="password"
+     placeholder="Password" 
+     name="password"
+     onChange={onChange} />
+  </Form.Group>
+  {/* Duda sobre boton y bootstrap*/}
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+
+     )
+}
